5fd7b22bc1542a9cea670a2040c30fbb823a2809
ndarugaa <ndaruga.francis@gmail.com>
2023-07-27 16:00:22 +0300

    store images, labels & masks in a data frame

---
+++ 2-loading_images_as_pd.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+import pprint
+
+ROOT_DIR = os.path.join('data', 'sliced_images')
+
+
+# -------------------READ FILE NAMES AS PANDAS DATA FRAAME-------------------------#
+
+def get_image_filenames():
+    """Gets a data frame of image filenames.
+
+    Returns:
+        A data frame of image filenames.
+    """
+
+    image_filenames = []
+    mask_filenames = []
+    label_filenames = []
+
+    for dir_name in os.listdir(ROOT_DIR):
+        for filename in os.listdir(os.path.join(ROOT_DIR, dir_name)):
+            if filename.endswith("_image.png"):
+                image_filenames.append(str(os.path.join(dir_name, filename)))
+            elif filename.endswith("_mask.png"):
+                mask_filenames.append(str(os.path.join(dir_name, filename)))
+            elif filename.endswith("_label.tif"):
+                label_filenames.append(str(os.path.join(dir_name, filename)))
+
+    df = pd.DataFrame({
+        "images": image_filenames,
+        "masks": mask_filenames,
+        "labels": label_filenames,
+    })
+    # print(df)
+    return df
+
+exit()
+
